d63761da10268979fec671eba4b847d416f52eb8
GreatGabbar <strivedi_be19@thapar.edu>
2021-04-03 19:17:22 +0530

    1/4 done

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 function App() {
   const classes = useStyles();
   const [state,setState] = useState({
--- src/components/location.js
+++ src/components/location.js
@@ -1,4 +1,4 @@
-import {Typography} from '@material-ui/core';
+import {Typography , Avatar} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {useState , useEffect} from 'react';
 
@@ -11,6 +11,14 @@ const useStyles = makeStyles((theme) => ({
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
+    avatar: {
+        display: 'flex',
+        justifyContent : "center",
+        textAlign : 'center',
+        '& > *': {
+          margin: theme.spacing(2),
+        }
+    },
   }));
 
 const Location =(props)=>{
@@ -21,17 +29,22 @@ const Location =(props)=>{
         setDate(date);
     },[])
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const val=date ? `${date.getHours()}:${date.getMinutes()}, ${months[date.getDate()]} ${date.getMonth()}` : null;
     return(
         <div className={classes.text}>
         <Typography variant="body1" color="secondary"> 
-            {date.getHours()}:{date.getMinutes()}, {months[date.getDate()]} {date.getMonth()}
+            {val}
         </Typography>
         <Typography variant="h5" component="h5">
             {props.data.cityName}, {props.data.country}
         </Typography>
+        <div className={classes.avatar}>
+        <Avatar src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`} />
         <Typography variant="h4" component="h5">
             {props.data.temp}&deg;C
         </Typography>
+
+        </div>
         <Typography variant="body2" component="h5">
            min : {props.data.temp_min}&deg;C / max : {props.data.temp_max}&deg;C
         </Typography>
